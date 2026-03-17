bf442fecb03d912a3f4b0499333b11ccd1c0d97b
yummjin <cho4u4o@gmail.com>
2026-03-17 18:27:59 +0900

    ✨ [feat] 면접 페이지에 개발 중 메시지 추가

--- src/pages/interview/InterviewPage.tsx
+++ src/pages/interview/InterviewPage.tsx
@@ -6,7 +6,9 @@ export default function InterviewPage() {
   return (
     <AppScreen>
       <Header left={<BackButton />} />
-      <div>Interview</div>
+      <div className="pt-header px-gutter text-gray-005 grid h-[80vh] w-full place-items-center">
+        모의 면접 기능은 아직 개발중이에요!
+      </div>
     </AppScreen>
   );
 }
