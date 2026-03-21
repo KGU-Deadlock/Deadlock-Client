1517da6b73c35b83ef511743328fc16a51506167
yummjin <cho4u4o@gmail.com>
2026-03-21 21:48:10 +0900

    ♻️ [refactor] 닉네임 최대 길이 13에서 8로 변경

--- src/utils/formatNicknameInput.ts
+++ src/utils/formatNicknameInput.ts
@@ -2,4 +2,4 @@ export function formatNicknameInput(value: string) {
   return value.trim().replace(/[^a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣]/g, "");
 }
 
-export const NICKNAME_MAX_LENGTH = 13;
+export const NICKNAME_MAX_LENGTH = 8;
