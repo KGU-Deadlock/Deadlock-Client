1f5828c7517d1c7e3aac83497f3fe5b6c5ba1f56
yummjin <cho4u4o@gmail.com>
2026-05-23 16:31:03 +0900

    📛 [rename] postKakaoLogin → getKakaoLogin 파일 및 함수명 변경

--- web/src/api/auth/getKakaoLogin.ts
+++ web/src/api/auth/getKakaoLogin.ts
@@ -4,11 +4,11 @@ import { END_POINTS } from "@/api/config/api-endpoints";
 
 import type { KakaoLoginResponse } from "./api.model";
 
-export function postKakaoLogin(
+export function getKakaoLogin(
   code: string,
 ): Promise<ApiResult<KakaoLoginResponse>> {
-  return baseApi.post<KakaoLoginResponse, { code: string }>(
-    END_POINTS.AUTH.TOKEN,
-    { code },
-  );
+  return baseApi.get<KakaoLoginResponse>(END_POINTS.AUTH.TOKEN, {
+    searchParams: { code },
+    headers: { Accept: "application/json" },
+  });
 }
